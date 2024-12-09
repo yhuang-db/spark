5d47581954a292079902d2adbcb922acc1cd0ac5
Yuchuan Huang <itisyuchuan@gmail.com>
2024-12-09 15:55:41 -0600

    test 1, 1, 300

--- sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/LargeRowBenchmark.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/execution/benchmark/LargeRowBenchmark.scala
@@ -47,8 +47,8 @@ object LargeRowBenchmark extends SqlBasedBenchmark {
       spark.udf.register("udf_upper", udf_upper(_: String): String)
 
       val benchmarks = Array(
-        Map("rows" -> 100, "cols" -> 10, "cellSize_mb" -> 1) //  OutOfMemory @ 100, 10, 2
-        //        Map("rows" -> 1, "cols" -> 1, "cellSize_mb" -> 250),
+        //        Map("rows" -> 100, "cols" -> 10, "cellSize_mb" -> 1) //  OutOfMemory @ 100, 10, 2
+        Map("rows" -> 1, "cols" -> 1, "cellSize_mb" -> 300)
         //        Map("rows" -> 1, "cols" -> 100, "cellSize_mb" -> 1),
         //        Map("rows" -> 1000, "cols" -> 1, "cellSize_mb" -> 1)
       )
