100e426191a74fe5f5b512e526f987a7f3bf6749
Yuchuan Huang <itisyuchuan@gmail.com>
2025-04-19 15:58:37 -0500

    new test suite

--- sql/core/src/test/scala/org/apache/spark/sql/DataFrameAggregateSuite.scala
+++ sql/core/src/test/scala/org/apache/spark/sql/DataFrameAggregateSuite.scala
@@ -17,7 +17,6 @@
 
 package org.apache.spark.sql
 
-import java.sql.{Date, Timestamp}
 import java.time.{Duration, LocalDateTime, Period}
 
 import scala.util.Random
@@ -1783,127 +1782,6 @@ class DataFrameAggregateSuite extends QueryTest
       "select max(transform(array(id), x -> x)), max(transform(array(id), x -> x)) from range(2)"
     )
     checkAnswer(res, Row(Array(1), Array(1)))
-  }
-
-  test("SPARK-xxxxx: DataSketches-based approx top-k tests") {
-    // Integer
-    val res1 = sql(
-      "SELECT approx_top_k(expr) FROM VALUES (0), (0), (1), (1), (2), (3), (4), (4) AS tab(expr);"
-    )
-    checkAnswer(res1, Row(Seq(Row(0, 2), Row(4, 2), Row(1, 2), Row(2, 1), Row(3, 1))))
-
-    // String
-    val res2 = sql(
-      "SELECT approx_top_k(expr, 2)" +
-        "FROM VALUES 'a', 'b', 'c', 'c', 'c', 'c', 'd', 'd' AS tab(expr);")
-    checkAnswer(res2, Row(Seq(Row("c", 4), Row("d", 2))))
-
-    val res3 = sql(
-      "SELECT approx_top_k(expr, 10, 100) FROM VALUES (0), (1), (1), (2), (2), (2) AS tab(expr);"
-    )
-    checkAnswer(res3, Row(Seq(Row(2, 3), Row(1, 2), Row(0, 1))))
-
-    // Boolean
-    val dfBool = Seq(true, true, false, true, true, false, false).toDF("expr")
-    dfBool.createOrReplaceTempView("t_bool")
-    val res4 = sql("SELECT approx_top_k(expr, 1) FROM t_bool;")
-    checkAnswer(res4, Row(Seq(Row(true, 4))))
-
-    // Byte
-    val res5 = sql(
-      "SELECT approx_top_k(expr, 2) " +
-        "FROM VALUES cast(0 AS BYTE), cast(0 AS BYTE), cast(1 AS BYTE), cast(1 AS BYTE), " +
-        "cast(2 AS BYTE), cast(3 AS BYTE), cast(4 AS BYTE), cast(4 AS BYTE) AS tab(expr);")
-    checkAnswer(res5, Row(Seq(Row(0, 2), Row(4, 2))))
-
-    // Short
-    val res6 = sql(
-      "SELECT approx_top_k(expr, 2) " +
-        "FROM VALUES cast(0 AS SHORT), cast(0 AS SHORT), cast(1 AS SHORT), cast(1 AS SHORT), " +
-        "cast(2 AS SHORT), cast(3 AS SHORT), cast(4 AS SHORT), cast(4 AS SHORT) AS tab(expr);")
-    checkAnswer(res6, Row(Seq(Row(0, 2), Row(4, 2))))
-
-    // Long
-    val res7 = sql(
-      "SELECT approx_top_k(expr, 2) " +
-        "FROM VALUES cast(0 AS LONG), cast(0 AS LONG), cast(1 AS LONG), cast(1 AS LONG), " +
-        "cast(2 AS LONG), cast(3 AS LONG), cast(4 AS LONG), cast(4 AS LONG) AS tab(expr);")
-    checkAnswer(res7, Row(Seq(Row(0, 2), Row(4, 2))))
-
-    // Float
-    val res8 = sql(
-      "SELECT approx_top_k(expr) " +
-        "FROM VALUES cast(0.0 AS FLOAT), cast(0.0 AS FLOAT), " +
-        "cast(1.0 AS FLOAT), cast(1.0 AS FLOAT), " +
-        "cast(2.0 AS FLOAT), cast(3.0 AS FLOAT), " +
-        "cast(4.0 AS FLOAT), cast(4.0 AS FLOAT) AS tab(expr);")
-    checkAnswer(res8, Row(Seq(Row(0.0, 2), Row(1.0, 2), Row(4.0, 2), Row(2.0, 1), Row(3.0, 1))))
-
-    // Double
-    val res9 = sql(
-      "SELECT approx_top_k(expr, 2) " +
-        "FROM VALUES cast(0.0 AS DOUBLE), cast(0.0 AS DOUBLE), " +
-        "cast(1.0 AS DOUBLE), cast(1.0 AS DOUBLE), " +
-        "cast(2.0 AS DOUBLE), cast(3.0 AS DOUBLE), " +
-        "cast(4.0 AS DOUBLE), cast(4.0 AS DOUBLE) AS tab(expr);")
-    checkAnswer(res9, Row(Seq(Row(0.0, 2), Row(4.0, 2))))
-
-    // Date
-    val res10 = sql(
-      "SELECT approx_top_k(expr, 2) " +
-        "FROM VALUES cast('2023-01-01' AS DATE), cast('2023-01-01' AS DATE), " +
-        "cast('2023-01-02' AS DATE), cast('2023-01-02' AS DATE), " +
-        "cast('2023-01-03' AS DATE), cast('2023-01-04' AS DATE), " +
-        "cast('2023-01-05' AS DATE), cast('2023-01-05' AS DATE) AS tab(expr);")
-    checkAnswer(
-      res10,
-      Row(Seq(Row(Date.valueOf("2023-01-02"), 2), Row(Date.valueOf("2023-01-01"), 2))))
-
-    // Timestamp
-    val res11 = sql(
-      "SELECT approx_top_k(expr, 2) " +
-        "FROM VALUES cast('2023-01-01 00:00:00' AS TIMESTAMP), " +
-        "cast('2023-01-01 00:00:00' AS TIMESTAMP), " +
-        "cast('2023-01-02 00:00:00' AS TIMESTAMP), " +
-        "cast('2023-01-02 00:00:00' AS TIMESTAMP), " +
-        "cast('2023-01-03 00:00:00' AS TIMESTAMP), " +
-        "cast('2023-01-04 00:00:00' AS TIMESTAMP), " +
-        "cast('2023-01-05 00:00:00' AS TIMESTAMP), " +
-        "cast('2023-01-05 00:00:00' AS TIMESTAMP) AS tab(expr);")
-    checkAnswer(
-      res11,
-      Row(Seq(Row(Timestamp.valueOf("2023-01-02 00:00:00"), 2),
-        Row(Timestamp.valueOf("2023-01-05 00:00:00"), 2))))
-
-    // Decimal
-    val res12 = sql(
-      "SELECT approx_top_k(expr, 2) AS top_k_result " +
-        "FROM VALUES (0.0), (0.0) ,(1.0), (1.0), (2.0), (3.0), (4.0), (4.0) AS tab(expr);")
-    checkAnswer(
-      res12,
-      Row(Seq(Row(new java.math.BigDecimal("0.0"), 2), Row(new java.math.BigDecimal("1.0"), 2))))
-
-    // Decimal
-    val res13 = sql(
-      "SELECT approx_top_k(expr, 2) AS top_k_result " +
-        "FROM VALUES CAST(0.0 AS DECIMAL(10, 2)), CAST(0.0 AS DECIMAL(10, 2)), " +
-        "CAST(1.0 AS DECIMAL(10, 2)), CAST(1.0 AS DECIMAL(10, 2)), " +
-        "CAST(2.0 AS DECIMAL(10, 2)), CAST(3.0 AS DECIMAL(10, 2)), " +
-        "CAST(4.0 AS DECIMAL(10, 2)), CAST(4.0 AS DECIMAL(10, 2)) AS tab(expr);")
-    checkAnswer(
-      res13,
-      Row(Seq(Row(new java.math.BigDecimal("0.00"), 2), Row(new java.math.BigDecimal("1.00"), 2))))
-
-    // Decimal
-    val res14 = sql(
-      "SELECT approx_top_k(expr, 2) AS top_k_result " +
-        "FROM VALUES CAST(0.0 AS DECIMAL(20, 1)), CAST(0.0 AS DECIMAL(20, 1)), " +
-        "CAST(1.0 AS DECIMAL(20, 1)), CAST(1.0 AS DECIMAL(20, 1)), " +
-        "CAST(2.0 AS DECIMAL(20, 1)), CAST(3.0 AS DECIMAL(20, 1)), " +
-        "CAST(4.0 AS DECIMAL(20, 1)), CAST(4.0 AS DECIMAL(20, 1)) AS tab(expr);")
-    checkAnswer(
-      res14,
-      Row(Seq(Row(new java.math.BigDecimal("0.0"), 2), Row(new java.math.BigDecimal("4.0"), 2))))
   }
 
   test("SPARK-16484: hll_*_agg + hll_union + hll_sketch_estimate positive tests") {
